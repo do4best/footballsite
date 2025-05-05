c6b864a982468b3e693e70f4f4003bac8187121a
do4best657 <panic_4_what@hotmail.com>
2025-05-05 14:08:22 +0500

    top Navebar Project is nearly complete

--- src/components/footaball/headfootball.jsx
+++ src/components/footaball/headfootball.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 import { RiTeamFill, RiTeamLine } from "react-icons/ri";
 import { IoFootball } from "react-icons/io5";
 import { TiLocation } from "react-icons/ti";
+import { FaLightbulb, FaRegLightbulb } from "react-icons/fa6";
+import { MdOutlineDarkMode } from "react-icons/md";
 function HeadFootball() {
   const [sideNave,setSideNave] = useState(false)
+  const [dark,setDark] = useState(localStorage.getItem('them')||'light')
+  useEffect(()=>{
+    localStorage.setItem('theme',dark)
+    document.querySelector('html').setAttribute('data-theme',dark)
+  },[dark])
+  const toggleTheme = ()=>{
+    setDark(dark === 'light'?'dark':'light')
+  }
     return (<>
     
     <div className="navbar bg-base-100 shadow-sm">
@@ -13,7 +23,7 @@ function HeadFootball() {
     <AiOutlineMenu size={25} onClick={()=>setSideNave(!sideNave)}/>
 
     {
-      sideNave ? (  <div className="bg-black/60 fixed w-full h-screen z-10 top-0 left-0"></div>):("")
+      sideNave ? (  <div className="bg-black/60 fixed w-full h-screen z-10 top-0 left-0" onClick={()=>setSideNave(!sideNave)}></div>):("")
     }
     <div className={sideNave ? "fixed top-0 left-0 w-[300px] h-screen text-gray-600 bg-white z-10 duration-200":"fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-200"}>
       <AiOutlineClose size={25} onClick={()=>setSideNave(!sideNave)} className='absolute right-4 top-4 cursor-pointer'/>
@@ -43,8 +53,14 @@ function HeadFootball() {
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">Irfan Football Club</a>
 
+    
+
   </div>
-  
+  <div className="flex gap-2">
+  <span className="   fixed top-2 right-35  sm:absolute sm:right-50 mt-2 " onClick={toggleTheme}>{!dark === 'light'?<FaRegLightbulb size={30}/>:<FaLightbulb size={30}/>} </span>
+    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    </div>
+
 
 </div>
     </>  );
